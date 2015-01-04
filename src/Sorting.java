5c549d1d0129ddbb54ba878b36d51dac2c2f0ad0
Malbaron0 <malbaron0@gmail.com>
2015-01-04 00:25:07 -0500

    Sorting

---
+++ src/Sorting.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Sorting {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		String para = "Paragraph.txt";
+		File file = new File(para);
+		Scanner input = new Scanner(file);
+		String words2[] = new String [100];
+		int count = 0;
+		String sentence = new String();
+		String temp = new String();
+		double average;
+		boolean swapped = true;
+		
+		
+		//Reads the paragraph from the file.
+		while (input.hasNextLine()){
+		sentence = sentence + input.nextLine();
+		}
+		
+		String words[] = sentence.split("\\.");
+		
+		while(swapped){
+			swapped = false;
+			
+			for (int i = 0; i < words.length-1; i++){
+				if(words[i].length() < words[i+1].length()){
+					temp = words[i];
+					words[i] = words[i+1];
+					words[i + 1] = temp;
+					swapped = true;
+				
+				}
+			
+				
+			}
+		}
+		
+		System.out.println("Sentences in order largest to smallest \n");
+		for (int i = 0; i < words.length-1; i++){
+			System.out.println((i+1)+":" + words[i]);
+		}
+		
+	}
+}
+	
